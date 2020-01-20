428b4e62c8ceb90f8ea56f0b045087f587d3ef75
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-01-20 13:34:27 +0200

    20.1.2020

--- learn_demonstration_policy.py
+++ learn_demonstration_policy.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import sys, os
 
-def create_model(n_features, n_labels, n_layers=2, n_nodes=64):
+def create_model(n_features, n_labels, n_layers=2, n_nodes=256):
     model = tf.keras.models.Sequential([tf.keras.Input(shape=(n_features,))])
     for i in range(n_layers):
         model.add(tf.keras.layers.Dense(n_nodes, activation='relu'))
@@ -54,8 +54,8 @@ if __name__ == '__main__':
 
     # train model
 
-    train_percentage = 0.8
-    train_idx = int(0.8 * n_samples)
+    train_percentage = 0.5
+    train_idx = int(train_percentage * n_samples)
     model = create_model(n_features, n_labels)
     epochs = 10000
     batch_size = 32
