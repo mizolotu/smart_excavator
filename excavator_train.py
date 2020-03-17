06e08d2b8e60061c8f094ced095912c56b2a5749
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-03-17 16:22:42 +0200

    17.3.2020

--- excavator_train.py
+++
@@ -1,4 +0,0 @@
-from excavator_server import autorun
-
-if __name__ == '__main__':
-    autorun('C:\\Users\\iotli\\Desktop\\Mevea-Data\\test_workspace.mws')
--- misc.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def moving_average(x, step=2, window=2):
-    seq = []
-    n = x.shape[0]
-    for i in np.arange(0, n, step):
-        idx = np.arange(np.maximum(0, i - window), np.minimum(n - 1, i + window + 1))
-        seq.append(np.mean(x[idx, :], axis=0))
-    return np.vstack(seq)
